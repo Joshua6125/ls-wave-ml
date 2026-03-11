e8b031b54fac6eef9c1138fe782ea4339cd96082
joshua <joshuavanrooij@gmail.com>
2026-03-11 14:16:51 +0100

    Add framework for testing loss functions

---
+++ tests/losses/__init__.py
@@ -0,0 +1,9 @@
+"""loss functions tests (PINN, Least Squares).
+
+Test modules:
+    - test_PINN: PINN loss tests
+    - test_LS: Least Squares loss tests
+
+Run with:
+    pytest tests/losses/
+"""
